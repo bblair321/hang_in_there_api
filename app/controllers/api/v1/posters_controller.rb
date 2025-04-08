27445a38e6e16afa1eddd394790012bf385f89d0
Brady Blair <bblair321@yahoo.comm>
2025-04-08 15:29:31 -0700

    Got our serilizer setup to handle a single poster and multiple posters.

--- app/controllers/api/v1/posters_controller.rb
+++ app/controllers/api/v1/posters_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::PostersController < ApplicationController
   def index
     posters = Poster.all
-    render json: { data: PosterSerializer.serialize_collection(posters) }
+    render json: { data: PosterSerializer.format_posters(posters) }
+  end
+
+  def show
+    poster = Poster.find(params[:id])
+    render json: { data: PosterSerializer.format(poster) }
   end
 end
--- app/serializers/poster_serializer.rb
+++ app/serializers/poster_serializer.rb
@@ -1,18 +1,32 @@
 class PosterSerializer
-  def self.serialize_collection(posters)
-    posters.each_with_object({}) do |poster, hash|
-      hash["id: #{poster.id}"] = {
-        type: "poster",
-        attributes: {
-          name: poster.name,
-          description: poster.description,
-          price: poster.price.to_f,
-          year: poster.year,
-          vintage: poster.vintage,
-          img_url: poster.img_url
-        }
+  def self.format_posters(posters)
+    posters.map do |poster|{
+      id: poster.id.to_s,
+      type: "poster",
+      attributes: {
+        name: poster.name,
+        description: poster.description,
+        price: poster.price,
+        year: poster.year,
+        vintage: poster.vintage,
+        img_url: poster.img_url
       }
+    } 
     end
+  end
+  def self.format(poster)
+    {
+      id: poster.id.to_s,
+      type: "poster",
+      attributes: {
+        name: poster.name,
+        description: poster.description,
+        price: poster.price,
+        year: poster.year,
+        vintage: poster.vintage,
+        img_url: poster.img_url
+      }
+    }
   end
 end
 
