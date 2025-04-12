6067b2cb905b35305137f944b086ae9152ac86a9
Maia Bard <Maiakbard@gmail.com>
2025-04-12 10:26:37 -0600

    Add updatded changes to poster price

--- spec/models/api/v1/poster/poster_model_spec.rb
+++ spec/models/api/v1/poster/poster_model_spec.rb
@@ -9,8 +9,8 @@ RSpec.describe "Posters queries", type: :model do
     Poster.create(name: "REGRET", description: "Hard work rarely pays off.", price: 89.00, year: 2018, vintage: true, img_url: "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d")
     Poster.create(name: "FAILURE", description: "Why bother trying? It's probably not worth it.", price: 68.00, year: 2019, vintage: true, img_url: "https://t3.ftcdn.net/jpg/11/70/36/90/240_F_1170369060_uUGb9y0Crkbn6hIHwRtWcCdpMfSDBaqv.jpg")
     Poster.create(name: "MEDIOCRITY",description: "Dreams are just that—dreams.",price: 75.00,year: 2021,vintage: false,img_url: "https://t3.ftcdn.net/jpg/10/94/43/46/240_F_1094434660_eWqgET75FkLFBMKBz7kd5E3dYIM7tgpO.jpg")
-    Poster.create(name: "MEDIOCRITY",description: "Dreams are just that—dreams.",price: 75.00,year: 2021,vintage: false,img_url: "https://t3.ftcdn.net/jpg/10/94/43/46/240_F_1094434660_eWqgET75FkLFBMKBz7kd5E3dYIM7tgpO.jpg")
-    Poster.create(name: "MEDIOCRITY",description: "Dreams are just that—dreams.",price: 75.00,year: 2021,vintage: false,img_url: "https://t3.ftcdn.net/jpg/10/94/43/46/240_F_1094434660_eWqgET75FkLFBMKBz7kd5E3dYIM7tgpO.jpg")
+    Poster.create(name: "MEDIOCRITY",description: "Dreams are just that—dreams.",price: 90.00,year: 2021,vintage: false,img_url: "https://t3.ftcdn.net/jpg/10/94/43/46/240_F_1094434660_eWqgET75FkLFBMKBz7kd5E3dYIM7tgpO.jpg")
+    Poster.create(name: "MEDIOCRITY",description: "Dreams are just that—dreams.",price: 55.00,year: 2021,vintage: false,img_url: "https://t3.ftcdn.net/jpg/10/94/43/46/240_F_1094434660_eWqgET75FkLFBMKBz7kd5E3dYIM7tgpO.jpg")
 
     id_first_made = Poster.first.id
     id_second_made = Poster.second.id
